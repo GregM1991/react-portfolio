c73c788c3a2e2a140d69c764c9c9fba775c487f1
Greg Martin <gregm31@live.com>
2020-12-06 12:01:25 +1100

    Initial Nav styling

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import utilStyles from "../styles/utils.module.css"
 import Link from "next/link"
+import Nav from "../components/Nav"
 
 import { getSortedPostsData } from "../lib/posts"
 
@@ -13,35 +14,36 @@ export async function getStaticProps() {
   }
 }
 
-export default function Home({ allPostsData }) {
-  return (
-    <>
-      <Head>
-        <title>Greg's Portfolio Site</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </>
-  )
-}
+const Home = ({ allPostsData }) => (
+  <>
+    <Head>
+      <title>Greg's Portfolio Site</title>
+    </Head>
+    <Nav />
+    <section className={utilStyles.headingMd}>
+      <p>[Your Self Introduction]</p>
+      <p>
+        (This is a sample website - you’ll be building a site like this on{" "}
+        <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+      </p>
+    </section>
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <h2 className={utilStyles.headingLg}>Blog</h2>
+      <ul className={utilStyles.list}>
+        {allPostsData.map(({ id, date, title }) => (
+          <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+        ))}
+      </ul>
+    </section>
+  </>
+)
+
+export default Home
---
+++ styles/colours.js
@@ -0,0 +1,5 @@
+export const colours = {
+  primaryMain: "#86d9b2",
+  primaryLight: "#e7f7f0",
+  primaryDark: "#1B5452",
+}
