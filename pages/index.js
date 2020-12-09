33cb4d99d2ed948c2bbe2153ef172981fa612f17
GregM1991 <gregm31@live.com>
2020-12-09 21:32:44 +0000

    Got rid of boiler plate

--- pages/index.js
+++ pages/index.js
@@ -20,29 +20,7 @@ const Home = ({ allPostsData }) => (
       <title>Greg's Portfolio Site</title>
     </Head>
     <Nav />
-    <section className={utilStyles.headingMd}>
-      <p>[Your Self Introduction]</p>
-      <p>
-        (This is a sample website - you’ll be building a site like this on{" "}
-        <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-      </p>
-    </section>
-    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-      <h2 className={utilStyles.headingLg}>Blog</h2>
-      <ul className={utilStyles.list}>
-        {allPostsData.map(({ id, date, title }) => (
-          <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
-        ))}
-      </ul>
-    </section>
+    <h1>Hello</h1>
   </>
 )
 
